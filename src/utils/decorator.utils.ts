e10fbff369275dc66e756644ddae8d3b5c55d934
Traian Anghel <tanghel@live.com>
2022-01-12 15:15:59 +0200

    banned types

--- src/utils/decorator.utils.ts
+++ src/utils/decorator.utils.ts
@@ -15,6 +15,7 @@ export class DecoratorUtils {
     };
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-types
   static getMethodDecorator<T>(type: Type<T>, target: Function): T | undefined {
     return this.getDecoratorOptions(type, target);
   }
@@ -23,6 +24,7 @@ export class DecoratorUtils {
     return this.getDecoratorOptions(type, target);
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-types
   private static getDecoratorOptions<T>(type: Type<T>, target: Object): T | undefined {
     const metadata = Reflect.getOwnMetadata(type.name, target);
     if (!metadata) {
--- src/utils/extensions/array.extensions.ts
+++ src/utils/extensions/array.extensions.ts
@@ -1,4 +1,4 @@
-Array.prototype.groupBy = function (predicate: Function, asArray = false) {
+Array.prototype.groupBy = function <T>(predicate: (item: T) => string, asArray = false) {
   let result = this.reduce(function (rv, x) {
     (rv[predicate(x)] = rv[predicate(x)] || []).push(x);
     return rv;
@@ -16,7 +16,7 @@ Array.prototype.groupBy = function (predicate: Function, asArray = false) {
   return result;
 };
 
-Array.prototype.selectMany = function (predicate: Function) {
+Array.prototype.selectMany = function <T, TOUT>(predicate: (item: T) => TOUT[]) {
   const result = [];
 
   for (const item of this) {
@@ -26,7 +26,7 @@ Array.prototype.selectMany = function (predicate: Function) {
   return result;
 };
 
-Array.prototype.firstOrUndefined = function (predicate?: Function) {
+Array.prototype.firstOrUndefined = function <T>(predicate?: (item: T) => boolean) {
   if (!predicate) {
     if (this.length > 0) {
       return this[0];
@@ -43,8 +43,8 @@ Array.prototype.firstOrUndefined = function (predicate?: Function) {
   return undefined;
 };
 
-Array.prototype.zip = function <TSecond, TResult>(second: TSecond[], predicate: Function): TResult[] {
-  return this.map((element: TSecond, index: number) => predicate(element, second[index]));
+Array.prototype.zip = function <T, TSecond, TResult>(second: TSecond[], predicate: (first: T, second: TSecond) => TResult): TResult[] {
+  return this.map((element: T, index: number) => predicate(element, second[index]));
 };
 
 Array.prototype.remove = function <T>(element: T): number {
