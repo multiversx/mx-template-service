2df146d0d1f880b484ae0a8f9d1d90b6112e85bf
tanghel <tanghel@live.com>
2024-04-25 16:12:08 +0300

    added config, schema, upgraded sdk-nestjs to latest version

---
+++ libs/common/src/config/configuration.ts
@@ -0,0 +1,19 @@
+// import { Config } from "@libs/entities/config/config";
+// import { ConfigurationLoader, ConfigurationLoaderSettings } from "@multiversx/sdk-nestjs-common";
+// import { join } from "path";
+
+// const CONFIG_DIRECTORY = '../../../../config/';
+// const YAML_CONFIG_FILENAME = CONFIG_DIRECTORY + 'config.yaml';
+// const CONFIG_SCHEMA_FILENAME = CONFIG_DIRECTORY + 'schema.yaml';
+
+// export function configuration(): Config {
+//   const configPath = join(__dirname, YAML_CONFIG_FILENAME);
+//   const schemaPath = join(__dirname, CONFIG_SCHEMA_FILENAME);
+
+//   const settings = new ConfigurationLoaderSettings({
+//     configPath,
+//     schemaPath,
+//   });
+
+//   return ConfigurationLoader.getConfiguration<Config>(settings);
+// }
